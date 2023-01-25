0f3724d6e0432f24fbf7d81c80e47210dcdcbf98
Abdullah2000Habib <abd3habib@gmail.com>
2023-01-25 18:05:27 +0200

    How to add sound to React app

--- src/components/Trivia/Trivia.jsx
+++ src/components/Trivia/Trivia.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
+import useSound from 'use-sound';
+import correctSounds from "../../assets/sounds/correct.mp3";
+import play from "../../assets/sounds/play.mp3";
+import wrongSounds from "../../assets/sounds/wrong.mp3";
 import { moneyPyramid } from '../Pyramid/Data';
 import { Answer, Answers, Question, TriviaWrapper } from './style';
 export default function Trivia({data,questionNumber,setStop,setQuestioinNumber,setEarned}) {
     const [question,setQuestion]=useState(null);
     const [selectAnswer,setSelectAnswer]=useState(null);
     const [beforAnimation,setBeforAnimation]=useState(0);
+    const [lestPlay] =useSound(play)
+    const [correctAnswer] =useSound(correctSounds)
+    const [wrongAnswer] =useSound(wrongSounds)
     useEffect(()=>{
         setQuestion(data[questionNumber-1])
     },[data,questionNumber]);
-
+    useEffect(()=>{  
+        lestPlay();
+    },[lestPlay]);
     const handleClick = (answer,e)=>{
         if(beforAnimation === 0){
         setBeforAnimation(answer.id);
@@ -17,18 +26,23 @@ export default function Trivia({data,questionNumber,setStop,setQuestioinNumber,s
             setSelectAnswer(answer)
             setTimeout(() => {
                 if(answer.correct){
+                    correctAnswer();
+                    setTimeout(() => {
                     setSelectAnswer(null)
                     setQuestioinNumber(prev=> prev + 1);
                     setBeforAnimation(0)
                     setEarned(Array.from(moneyPyramid).reverse()[questionNumber-1].amount)
+                    }, 3000);
                 }else{
+                    setTimeout(() => { 
+                    wrongAnswer();
                     setSelectAnswer(null)
                     setQuestioinNumber(1);
                     setBeforAnimation(0)
                     setStop(true)
+                    }, 3000);
                 }
-
-            }, 3010);
+            }, 3002);
         }, 3000 );
         }
     }
